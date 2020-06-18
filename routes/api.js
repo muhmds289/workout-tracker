f8749b8185936c08ab218a507701cef1167a6cfa
Mohamed Ibrahim <muhmds@hotmail.com>
2020-06-17 22:47:10 -0400

    2nd commit after fixing git issues

---
+++ routes/api.js
@@ -0,0 +1,74 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+console.log("api routes", Workout)
+
+router.post("/api/workouts", (req, res) => {
+    console.log("post")
+    Workout.create({})
+        .then(dbWorkout => {
+            console.log("data frome post", dbWorkout)
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            console.log("error", err)
+            res.json(err);
+        });
+});
+
+router.put("/api/workouts/:id", ({
+    body,
+    params
+}, res) => {
+    Workout.findByIdAndUpdate(
+            params.id, {
+                $push: {
+                    exercises: body
+                }
+            }, {
+                new: true,
+                runValidators: true
+            }
+        )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .limit(7)
+        .then(dbWorkouts => {
+            console.log(dbWorkouts);
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.delete("/api/workouts", ({
+    body
+}, res) => {
+    Workout.findByIdAndDelete(body.id)
+        .then(() => {
+            res.json(true);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+module.exports = router;
